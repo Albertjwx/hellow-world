92c8518526290199398bf60c419d5c55f0dee2a2
1 <1@qq.com>
2019-01-14 08:46:43 +0800

    增加一个界面按钮

--- SmobilerApplication1/SmobilerForm1.Designer.cs
+++ SmobilerApplication1/SmobilerForm1.Designer.cs
@@ -21,6 +21,7 @@ namespace SmobilerApplication1
         {
             this.buttonBlueState = new Smobiler.Core.Controls.Button();
             this.buttonOpenBlue = new Smobiler.Core.Controls.Button();
+            this.button1 = new Smobiler.Core.Controls.Button();
             // 
             // buttonBlueState
             // 
@@ -38,11 +39,20 @@ namespace SmobilerApplication1
             this.buttonOpenBlue.Text = "打开蓝牙界面";
             this.buttonOpenBlue.Press += new System.EventHandler(this.buttonOpenBlue_Press);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.Text = "打开蓝牙界面";
+            this.button1.Press += new System.EventHandler(this.buttonOpenBlue_Press);
+            // 
             // SmobilerForm1
             // 
             this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
             this.buttonBlueState,
-            this.buttonOpenBlue});
+            this.buttonOpenBlue,
+            this.button1});
             this.Name = "SmobilerForm1";
 
         }
@@ -50,5 +60,6 @@ namespace SmobilerApplication1
 
         private Smobiler.Core.Controls.Button buttonBlueState;
         private Smobiler.Core.Controls.Button buttonOpenBlue;
+        private Smobiler.Core.Controls.Button button1;
     }
 }
