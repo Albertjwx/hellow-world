771f7cbed2ea8ac8057ffeacb2c28473615770d4
1 <1@qq.com>
2019-01-14 08:41:51 +0800

    创建工程

---
+++ SmobilerApplication1/SmobilerForm1.Designer.cs
@@ -0,0 +1,54 @@
+﻿using Smobiler.Core;
+
+namespace SmobilerApplication1
+{
+    partial class SmobilerForm1 : Smobiler.Core.Controls.MobileForm
+    {
+        #region "SmobilerForm Designer generated code "
+
+        //SmobilerForm overrides dispose to clean up the component list.
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+
+        //NOTE: The following procedure is required by the SmobilerForm Designer
+        //It can be modified using the SmobilerForm Designer.  
+        //Do not modify it using the code editor.
+        [System.Diagnostics.DebuggerStepThrough()]
+        private void InitializeComponent()
+        {
+            this.buttonBlueState = new Smobiler.Core.Controls.Button();
+            this.buttonOpenBlue = new Smobiler.Core.Controls.Button();
+            // 
+            // buttonBlueState
+            // 
+            this.buttonBlueState.Location = new System.Drawing.Point(100, 45);
+            this.buttonBlueState.Name = "buttonBlueState";
+            this.buttonBlueState.Size = new System.Drawing.Size(100, 30);
+            this.buttonBlueState.Text = "获取蓝牙状态";
+            this.buttonBlueState.Press += new System.EventHandler(this.buttonBlueState_Press);
+            // 
+            // buttonOpenBlue
+            // 
+            this.buttonOpenBlue.Location = new System.Drawing.Point(100, 120);
+            this.buttonOpenBlue.Name = "buttonOpenBlue";
+            this.buttonOpenBlue.Size = new System.Drawing.Size(100, 30);
+            this.buttonOpenBlue.Text = "打开蓝牙界面";
+            this.buttonOpenBlue.Press += new System.EventHandler(this.buttonOpenBlue_Press);
+            // 
+            // SmobilerForm1
+            // 
+            this.Controls.AddRange(new Smobiler.Core.Controls.MobileControl[] {
+            this.buttonBlueState,
+            this.buttonOpenBlue});
+            this.Name = "SmobilerForm1";
+
+        }
+        #endregion
+
+        private Smobiler.Core.Controls.Button buttonBlueState;
+        private Smobiler.Core.Controls.Button buttonOpenBlue;
+    }
+}
---
+++ SmobilerApplication1/SmobilerForm1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smobiler.Core;
+using Smobiler.Core.Controls;
+
+namespace SmobilerApplication1
+{
+    partial class SmobilerForm1 : Smobiler.Core.Controls.MobileForm
+    {
+        public SmobilerForm1() : base()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBlueState_Press(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Client.Bluetooth.IsOpen((object obj, Smobiler.Core.RPC.BluetoothManagerIsOpenArgs args) =>
+                {
+                    if (args.IsOpen == true)
+                        Toast("Open");
+                    else
+                        Toast("Not Open");
+                });
+            }
+            catch (Exception ex)
+            {
+                Toast(ex.Message, ToastLength.SHORT);
+            }
+            this.Client.Bluetooth.GetHashCode();
+        }
+
+        private void buttonOpenBlue_Press(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Client.Bluetooth.Show();
+            }
+            catch(Exception ex)
+            {
+                Toast(ex.Message, ToastLength.SHORT);
+            }
+        }
+    }
+}
